eb388984cdbe6f6d0fef587f8af9f9584d994a26
seb75019 <60775503+seb75019@users.noreply.github.com>
2023-11-29 16:29:54 +0100

    Add files via upload

---
+++ public/calendar.js
@@ -0,0 +1,275 @@
+function test1() {
+  let date1 = Date.parse("2023-12-01T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide1").style.display = "none";
+    document.getElementById("show1").style.display = "block";
+    document.getElementById("other1").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test2() {
+  let date1 = Date.parse("2023-12-02T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide2").style.display = "none";
+    document.getElementById("show2").style.display = "block";
+    document.getElementById("other2").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test3() {
+  let date1 = Date.parse("2023-12-03T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide3").style.display = "none";
+    document.getElementById("show3").style.display = "block";
+    document.getElementById("other3").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test4() {
+  let date1 = Date.parse("2023-12-04T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide4").style.display = "none";
+    document.getElementById("show4").style.display = "block";
+    document.getElementById("other4").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test5() {
+  let date1 = Date.parse("2023-12-05T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide5").style.display = "none";
+    document.getElementById("show5").style.display = "block";
+    document.getElementById("other5").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test6() {
+  let date1 = Date.parse("2023-12-06T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide6").style.display = "none";
+    document.getElementById("show6").style.display = "block";
+    document.getElementById("other6").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test7() {
+  let date1 = Date.parse("2023-12-07T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide7").style.display = "none";
+    document.getElementById("show7").style.display = "block";
+    document.getElementById("other7").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test8() {
+  let date1 = Date.parse("2023-12-08T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide8").style.display = "none";
+    document.getElementById("show8").style.display = "block";
+    document.getElementById("other8").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test9() {
+  let date1 = Date.parse("2023-12-09T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide9").style.display = "none";
+    document.getElementById("show9").style.display = "block";
+    document.getElementById("other9").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test10() {
+  let date1 = Date.parse("2023-12-10T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide10").style.display = "none";
+    document.getElementById("show10").style.display = "block";
+    document.getElementById("other10").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test11() {
+  let date1 = Date.parse("2023-12-11T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide11").style.display = "none";
+    document.getElementById("show11").style.display = "block";
+    document.getElementById("other11").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test12() {
+  let date1 = Date.parse("2023-12-12T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide12").style.display = "none";
+    document.getElementById("show12").style.display = "block";
+    document.getElementById("other12").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test13() {
+  let date1 = Date.parse("2023-12-13T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide13").style.display = "none";
+    document.getElementById("show13").style.display = "block";
+    document.getElementById("other13").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test14() {
+  let date1 = Date.parse("2023-12-14T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide14").style.display = "none";
+    document.getElementById("show14").style.display = "block";
+    document.getElementById("other14").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test15() {
+  let date1 = Date.parse("2023-12-15T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide15").style.display = "none";
+    document.getElementById("show15").style.display = "block";
+    document.getElementById("other15").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test16() {
+  let date1 = Date.parse("2023-12-16T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide16").style.display = "none";
+    document.getElementById("show16").style.display = "block";
+    document.getElementById("other16").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test17() {
+  let date1 = Date.parse("2023-12-17T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide17").style.display = "none";
+    document.getElementById("show17").style.display = "block";
+    document.getElementById("other17").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test18() {
+  let date1 = Date.parse("2023-12-18T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide18").style.display = "none";
+    document.getElementById("show18").style.display = "block";
+    document.getElementById("other18").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test19() {
+  let date1 = Date.parse("2023-12-19T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide19").style.display = "none";
+    document.getElementById("show19").style.display = "block";
+    document.getElementById("other19").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test20() {
+  let date1 = Date.parse("2023-12-20T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide20").style.display = "none";
+    document.getElementById("show20").style.display = "block";
+    document.getElementById("other20").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test21() {
+  let date1 = Date.parse("2023-12-21T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide21").style.display = "none";
+    document.getElementById("show21").style.display = "block";
+    document.getElementById("other21").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test22() {
+  let date1 = Date.parse("2023-12-22T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide22").style.display = "none";
+    document.getElementById("show22").style.display = "block";
+    document.getElementById("other22").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test23() {
+  let date1 = Date.parse("2023-12-23T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide23").style.display = "none";
+    document.getElementById("show23").style.display = "block";
+    document.getElementById("other23").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test24() {
+  let date1 = Date.parse("2023-12-24T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide24").style.display = "none";
+    document.getElementById("show24").style.display = "block";
+    document.getElementById("other24").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
+function test25() {
+  let date1 = Date.parse("2023-12-25T00:00:00.000+01:00");
+  let today = new Date();
+  if (today >= date1) {
+    document.getElementById("hide25").style.display = "none";
+    document.getElementById("show25").style.display = "block";
+    document.getElementById("other25").style.display = "block";
+  } else {
+    location.href = "./public/message.html";
+  }
+}
